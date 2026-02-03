b1ea9619feb655772178d67700e74cddb8662bec
JeevanReddy <jeevanjgfr@gmail.com>
2026-02-03 23:07:58 +0530

    Fix routes path for render

--- models/userModel.js
+++ models/userModel.js
@@ -22,11 +22,12 @@ const UserSchema=new mongoose.Schema(
       { timestamps: true }
 )
 
-UserSchema.pre("save",async function (next) {
-    if(!this.isModified('password'))return next();
-    const salt=await bcrypt.genSalt(10);
-    this.password=await bcrypt.hash(this.password,salt)
-    next()
-})
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 
 module.exports=mongoose.model('User',UserSchema)
