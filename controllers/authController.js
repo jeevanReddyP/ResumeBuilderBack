c981311b1e2e51cd6c8a10229b6e8b12879451de
JeevanReddy <jeevanjgfr@gmail.com>
2026-02-03 22:02:12 +0530

    Fix routes path for render

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,15 +2,14 @@ const express=require("express")
 const User=require("../models/userModel")
 const bcrypt=require("bcryptjs")
 const jwt = require("jsonwebtoken");
-
 const RegisterUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
+console.log("Hello")
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
+console.log("JWT_SECRET VALUE:", process.env.JWT_SECRET);
     const existing = await User.findOne({ email });
     if (existing) {
       return res.status(400).json({ message: "Email already registered" });
